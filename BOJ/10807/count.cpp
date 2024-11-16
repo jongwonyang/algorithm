7c21cf451703ddd967c566bd6c0304368fc8de0c
양종원 <yjw963@gmail.com>
2024-11-16 15:30:27 +0900

    [BOJ] 10807 - 전역변수로 수정

--- BOJ/10807/count.cpp
+++ BOJ/10807/count.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N, num, arr[201];
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N;
     cin >> N;
 
-    int num, count[201] = { 0 };
     for (int i = 0; i < N; i++)
     {
         cin >> num;
-        count[num + 100]++;
+        arr[num + 100]++;
     }
 
-    int v;
-    cin >> v;
-
-    cout << count[v + 100];
+    cin >> num;
+    cout << arr[num + 100];
 }
