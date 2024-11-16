510e45586c765397c7d42f888da6a5c6b4fa9f08
양종원 <yjw963@gmail.com>
2024-11-16 13:51:22 +0900

    [BOJ] 15552 - 빠른 A+B 해결

---
+++ BOJ/15552/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int T, A, B;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A >> B;
+        cout << A + B << '\n';
+    }
+}
