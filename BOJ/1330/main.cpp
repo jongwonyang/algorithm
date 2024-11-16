fbfc9fc07a3763e315f6d760cf9e2c98e7c8cebd
양종원 <yjw963@gmail.com>
2024-11-16 11:34:21 +0900

    [BOJ] 1330 - 두 수 비교하기 해결

---
+++ BOJ/1330/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    if (A > B)
+        cout << ">";
+    else if (A < B)
+        cout << "<";
+    else
+        cout << "==";
+}
