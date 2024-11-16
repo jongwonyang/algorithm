41c7f50f575075659e9f7be4d8806b26e9e0a63c
양종원 <yjw963@gmail.com>
2024-11-16 12:19:08 +0900

    [BOJ] 10950 - A+B - 3 해결

---
+++ BOJ/10950/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T, A, B;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A >> B;
+        cout << A + B << '\n';
+    }
+}
