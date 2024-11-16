37d480b11319346edb9b421265d152cec28242f9
양종원 <yjw963@gmail.com>
2024-11-16 12:26:38 +0900

    [BOJ] 10952 - A+B - 5 해결

---
+++ BOJ/10952/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+
+    while (true)
+    {
+        cin >> A >> B;
+        if (A == 0 and B == 0) break;
+        cout << A + B << '\n';
+    }
+}
